4bb2f6af6f3f90e61dd44d33d405b54d8aee6130
Jack Sung <magicsung@gmail.com>
2015-10-13 17:57:58 +0800

    add admin & layout

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
 
   root "tipsy#index"
 
+  namespace :admin do
+    root "store#index"
+    resources :store
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
