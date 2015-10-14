cd4319afb70b2af1c2665c46edeceb8b30a15d9f
Jack Sung <magicsung@gmail.com>
2015-10-14 10:53:38 +0800

    add admin::store

--- app/controllers/admin/store_controller.rb
+++ app/controllers/admin/store_controller.rb
@@ -7,5 +7,45 @@ class Admin::StoreController < ApplicationController
     
   end
 
+  def show
+    
+  end
+
+  def new
+    @store = Store.new
+  end
+
+  def create
+
+    @store = Store.new( store_params )
+
+    if @store.save
+      flash[:notice] = "Store was successfully created!"
+      redirect_to :action => :index 
+    else
+      flash.now[:alert] = "Please enter something!"
+      render "new"
+    end
+    
+  end
+
+  def edit
+    
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
+  end
+
+
+  private
+
+  def store_params
+    params.require(:store).permit(:name, :description)
+  end
 
 end
