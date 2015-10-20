f4e3331d90291ee73982a037a4d2262668011e25
Jack Sung <magicsung@gmail.com>
2015-10-20 11:42:12 +0800

    install rspec.

--- test/controllers/admin/dj_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class Admin::DjControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/controllers/admin/user_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class Admin::UserControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
