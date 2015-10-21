71ca0254bbe1c9d41683661ac7b6b02921fa95b5
Jack Sung <magicsung@gmail.com>
2015-10-21 09:53:06 +0800

    create store-show, update nav bar.

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -3,17 +3,19 @@ class StoresController < ApplicationController
   before_action :find_store, :only => [ :show ]
 
   def index
-    if params[:keyword] 
-      @store = Store.where( [ "name like ?", "%#{params[:keyword]}%" ] )
-    else
-      @store = Store.all
-    end
+    # if params[:keyword] 
+    #   @store = Store.where( [ "name like ?", "%#{params[:keyword]}%" ] )
+    # else
+    #   @store = Store.all
+    # end
+    @store = Store.all
+    @category = params[:category].to_i
 
-    @store = @store.page(params[:page]).per(10)
+    # @store = @store.page(params[:page]).per(10)
   end
 
   def show
-    @store = params[:id]
+    
   end
 
 
