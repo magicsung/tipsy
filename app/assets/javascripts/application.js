9d3d2553ac8288a8c3eed6fe03f8b9ea2062490e
Jack Sung <magicsung@gmail.com>
2015-10-21 15:33:54 +0800

    fix masonry.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -30,24 +30,12 @@ $(document).on('page:change', function(){
     return $('#masonry-container').imagesLoaded(function() {
       return $('#masonry-container').masonry({
         itemSelector: '.box',
-        isFitWidth: true,
-        columnWidth: 10,
-        gutterWidth: 10,
-        columnWidth: function(containerWidth) {
-          if ($(window).width() > 1200) {
-            return containerWidth / 6;
-          } else if ($(window).width() > 1000 && $(window).width() <= 1200) {
-            return containerWidth / 5;
-          } else if ($(window).width() > 800 && $(window).width() <= 1000) {
-            return containerWidth / 4;
-          } else if ($(window).width() > 400 && $(window).width() <= 800) {
-            return containerWidth / 3;
-          } else {
-            return containerWidth / 1;
-          }
-        }
+        isFitWidth: true
       });
     });
   });
 
 });
+
+
+
