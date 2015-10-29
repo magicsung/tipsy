14611df74fa10364813aa55113d1d95d722d1811
Jack Sung <magicsung@gmail.com>
2015-10-29 13:48:08 +0800

    update devise view.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
-  layout :layout_by_resource
-
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_contact_form
 
@@ -12,14 +9,6 @@ class ApplicationController < ActionController::Base
 
   def set_contact_form
     @contact = Contact.new
-  end
-
-  def layout_by_resource
-    if devise_controller?
-      "admin"
-    else
-      "application"
-    end
   end
 
   def configure_permitted_parameters
