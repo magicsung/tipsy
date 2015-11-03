75a4f98388dd71583741c61e0a09d27d1c077b24
Jack Sung <magicsung@gmail.com>
2015-11-03 19:49:51 +0800

    fix paperclip resize.

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,8 +3,8 @@ class Event < ActiveRecord::Base
   has_many :event_djships
   has_many :djs, :through => :event_djships
 
-  has_attached_file :edm, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  has_attached_file :edm, :storage => :s3, :s3_credentials => "#{Rails.root}/config/s3.yml", :s3_host_name => "s3-ap-northeast-1.amazonaws.com"
+  has_attached_file :edm, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png",
+                    :storage => :s3, :s3_credentials => "#{Rails.root}/config/s3.yml", :s3_host_name => "s3-ap-northeast-1.amazonaws.com"
   validates_attachment_content_type :edm, content_type: /\Aimage\/.*\Z/
 
   validates_presence_of :title
--- app/models/store.rb
+++ app/models/store.rb
@@ -6,8 +6,8 @@ class Store < ActiveRecord::Base
 
   validates_presence_of :name
 
-  has_attached_file :photo, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  has_attached_file :photo, :storage => :s3, :s3_credentials => "#{Rails.root}/config/s3.yml", :s3_host_name => "s3-ap-northeast-1.amazonaws.com"
+  has_attached_file :photo, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png",
+                    :storage => :s3, :s3_credentials => "#{Rails.root}/config/s3.yml", :s3_host_name => "s3-ap-northeast-1.amazonaws.com"
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
 
 end
