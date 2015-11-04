00e365cbc4be5795cb028e3b1c3727b145298eac
Jack Sung <magicsung@gmail.com>
2015-11-04 20:49:22 +0800

    fix QRcode determine.

--- app/controllers/vips_controller.rb
+++ app/controllers/vips_controller.rb
@@ -4,7 +4,7 @@ class VipsController < ApplicationController
   def index
     @order = Order.new
 
-    if current_user && current_user.orders.exists? && current_user.orders.joins(:trades).where('trades.paid = true')
+    if current_user && current_user.orders.exists? && current_user.orders.joins(:trades).where('trades.paid = true').exists?
 
       order = current_user.orders.joins(:trades).where('trades.paid = true').last
       # @qrcode = RQRCode::QRCode.new( order, :size => 4, :level => :h )
