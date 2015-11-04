03a18dfa127d24ef4372264b875c064b3ec8d374
Jack Sung <magicsung@gmail.com>
2015-11-04 16:54:16 +0800

    Fix qrcode, show date time.

--- app/controllers/vips_controller.rb
+++ app/controllers/vips_controller.rb
@@ -6,10 +6,11 @@ class VipsController < ApplicationController
 
     if current_user.orders.exists? && current_user.orders.joins(:trades).where('trades.paid = true')
 
-      order_code = current_user.orders.joins(:trades).where('trades.paid = true').last.qrcode
-      # @qrcode = RQRCode::QRCode.new( order_code, :size => 4, :level => :h )
-      @qrcode = RQRCode::QRCode.new(order_code).to_img.resize(200, 200).to_data_url
-
+      order = current_user.orders.joins(:trades).where('trades.paid = true').last
+      # @qrcode = RQRCode::QRCode.new( order, :size => 4, :level => :h )
+      @order_info = order
+      @qrcode = RQRCode::QRCode.new(order.qrcode).to_img.resize(200, 200).to_data_url
+      
     end
   end
 
