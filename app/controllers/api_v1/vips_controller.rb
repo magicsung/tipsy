35880eacf532811086271a05e608dac4b046378a
Jack Sung <magicsung@gmail.com>
2015-11-04 22:38:25 +0800

    update qrcode confirm.

--- app/controllers/api_v1/vips_controller.rb
+++ app/controllers/api_v1/vips_controller.rb
@@ -5,7 +5,7 @@ class ApiV1::VipsController < ApiController
     if @user
 
       qrcode = params[:qrcode]
-      if Order.find_by_qrcode(qrcode) && Order.find_by_qrcode(qrcode).user == @user
+      if Order.find_by_qrcode(qrcode)
         render :json => { :message => "confirm successfully!" },  :status => 200
       else
         render :json => { :message => "confirm faild!" },  :status => 500
